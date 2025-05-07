fdda839ac04ae0279841c7466e282acf064e96a0
geniusjoelraj <geniusjoel2005@gmail.com>
2025-05-07 23:26:16 +0530

    some changes and added README

--- main.py
+++ main.py
@@ -2,15 +2,16 @@ from PIL import Image, ImageOps
 
 with Image.open("/home/tensai/Downloads/Ado's best adobum cover.webp") as im:
     im=ImageOps.grayscale(im)
-    im=im.resize((30,30))
+    im=im.resize((50,50))
+    im=im.rotate(270)
     ascii="#$%*(/=+_~-`,."
     arr=[]
-    for i in range(30):
-        for j in range(30):
+    for i in range(49,0,-1):
+        for j in range(50):
             intensity=im.getpixel((i,j))
             assert isinstance(intensity, int)
             index=intensity*13//255
-            arr.append(ascii[index])
+            arr.append(ascii[index]+" ")
         arr.append("\n")
     image="".join(arr)
 print(image)
