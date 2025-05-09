cfda464a1837e3d9123365429f62b12eee5cdf94
geniusjoelraj <geniusjoel2005@gmail.com>
2025-05-09 14:57:32 +0530

    can generate for non square images now

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
 from PIL import Image, ImageOps
 
-quality=20
-kanji="鬱森冊花代日三二一丶"
+quality=100
+kanji="鬱森冊花代日三二一丶 "
 ascii="#$%*(/=+_~-`,."
-kanji_inverse="丶一二三日代花冊森鬱"
+kanji_inverse=" 丶一二三日代花冊森鬱"
 chars=kanji_inverse
-with Image.open("/home/tensai/Pictures/archlinux-icon.png") as im:
-    im=ImageOps.grayscale(im)
-    im=im.resize((quality,quality))
-    im=im.rotate(270)
 
+def resize_img(img):
+    width, height=img.size
+    aspect_ratio=width/height
+    height=quality
+    width=int(height*aspect_ratio)
+    return img.resize((width, height))
+
+with Image.open("/home/tensai/Pictures/why-do-people-believe-gear-5-was-planned-since-chapter-one-v0-2frel64qveja1.webp") as im:
+    im=im.rotate(90,expand=True)
+    im=im.transpose(method=Image.Transpose.FLIP_TOP_BOTTOM)
+    im=ImageOps.grayscale(im)
+    im=resize_img(im)
     arr=[]
-    for i in range(quality-1,0,-1):
-        for j in range(quality):
+    w,h=im.size
+    for i in range(w):
+        for j in range(h):
             intensity=im.getpixel((i,j))
             assert isinstance(intensity, int)
             index=intensity*(len(chars)-1)//255
-            arr.append(chars[index]+"")
+            if chars[index]==" ":
+                arr.append(" ")
+            arr.append(chars[index])
         arr.append("\n")
     image="".join(arr)
 print(image)
+
