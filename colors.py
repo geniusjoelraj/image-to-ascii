679d134016cb2f0e73112993d142d68246999110
geniusjoelraj <geniusjoel2005@gmail.com>
2025-05-23 22:19:15 +0530

    added color to ascii art

---
+++ colors.py
@@ -0,0 +1,12 @@
+intensity_max = "\033[0;30m"
+intensity_low = "\033[0;37m"
+intensity_medium = "\033[1;30m"
+light_white = "\033[1;37m"
+white=""
+
+colors = [intensity_max, intensity_medium, intensity_low, light_white, light_white, white]
+
+end = "\033[0m"
+def rgb_to_ansi(rgb):
+    r, g, b, *a=rgb
+    return f"\x1b[38;2;{r};{g};{b}m"
